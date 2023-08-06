990174a5f4de645b5589fa6fe9298f3be974bc1f
vinayaksai2711 <100204538+vinayaksai2711@users.noreply.github.com>
2023-08-06 14:37:10 +0000

    updated server code in comments.js

--- comments.js
+++ comments.js
@@ -1,31 +1,38 @@
-//create node express web server api
-// 1. load express
-const express = require("express");
-// 2. create app object
-const app = express();
-// 3. create port
-const port = 3000;
-// 4. create server
-app.listen(port, () => {
-  console.log("server is running on port 3000");
+//create web server
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function(request, response){
+	console.log('Connection');
+	var path = url.parse(request.url).pathname;
+	switch(path){
+		case '/':
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write('hello world');
+			response.end();
+			break;
+		case '/socket.html':
+			fs.readFile(__dirname + path, function(error, data){
+				if(error){
+					response.writeHead(404);
+					response.write("opps this doesn't exist - 404");
+					response.end();
+				}
+				else{
+					response.writeHead(200, {"Content-Type": "text/html"});
+					response.write(data, "utf8");
+					response.end();
+				}
+			});
+			break;
+		default:
+			response.writeHead(404);
+			response.write("opps this doesn't exist - 404");
+			response.end();
+			break;
+	}
 });
-// 5. create router
-app.get("/comments", (req, res) => {
-  res.send("all comments");
-});
-// 6. create router
-app.get("/comments/:id", (req, res) => {
-  res.send("comment with id");
-});
-// 7. create router
-app.post("/comments", (req, res) => {
-  res.send("create new comment");
-});
-// 8. create router
-app.put("/comments/:id", (req, res) => {
-  res.send("update comment with id");
-});
-// 9. create router
-app.delete("/comments/:id", (req, res) => {
-  res.send("delete comment with id");
-});
+
+server.listen(8001);
