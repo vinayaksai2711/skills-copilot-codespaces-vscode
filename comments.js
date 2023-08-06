307012955fc959e1af9ffdca23c9acc9620a16b2
vinayaksai2711 <100204538+vinayaksai2711@users.noreply.github.com>
2023-08-06 11:22:56 +0000

    Copilot third commit 9

--- comments.js
+++ comments.js
@@ -1,20 +1,10 @@
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//create a web server
+const http = require('http');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello, world!</h1>');
+});
 
-// connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/express-demo', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.log('Could not connect to MongoDB...', err));
-
-// create schema
-const courseSchema = new mongoose.Schema({
-    name: String, });
+server.listen(8000);
+console.log('Server is running on port 8000');
